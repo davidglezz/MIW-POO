1be6cbcf6e8cfc38d426ee86b4af4dcc98b28aec
davidglezz <davidglezz@outlook.es>
2018-01-09 14:09:59 +0100

    Integrate Schema classes

--- php/public/index.php
+++ php/public/index.php
@@ -5,6 +5,19 @@ use \Psr\Http\Message\ResponseInterface as Response;
 require '../vendor/autoload.php';
 
 $app = new \Slim\App;
+
+$schema = new POO\SchemaReader('schema/schema.jsonld');
+$validator = new POO\SchemaValidator($schema);
+
+$app->get('/', function (Request $request, Response $response, array $args) {
+
+    return $response->withJSON(
+        $schema->getTypes(),
+        200,
+        JSON_UNESCAPED_UNICODE
+    );
+});
+
 $app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
     $name = $args['name'];
     $response->getBody()->write("Hello, $name");
