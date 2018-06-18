02b9eb74899482eb10efb2ddb1c7b41aab52acc3
davidglezz <davidglezz@outlook.es>
2018-06-18 23:53:00 +0200

    Fix cors

--- php/public/index.php
+++ php/public/index.php
@@ -11,8 +11,8 @@ $app->add(function ($req, $res, $next) {
     $response = $next($req, $res);
     return $response
             ->withHeader('Access-Control-Allow-Origin', '*')
-            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
-            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+            ->withHeader('Access-Control-Allow-Headers', '*')
+            ->withHeader('Access-Control-Allow-Methods', '*');
 });
 
 $app->options('/{routes:.+}', function ($request, $response, $args) {
