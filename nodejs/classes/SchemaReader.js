a92b396db55be1d2282aa8d83ae39bdf9eb7b0e6
davidglezz <davidglezz@outlook.es>
2018-06-19 17:15:38 +0200

    JS: implement SchemaReader class

--- nodejs/classes/SchemaReader.js
+++ nodejs/classes/SchemaReader.js
@@ -1,6 +1,53 @@
-// const settings = require('./settings.json')
+const fs = require('fs')
 
 module.exports = class SchemaReader {
-  constructor () {
+  constructor (filepath) {
+    this.graph = {}
+    fs.readFile(filepath, 'utf8', (err, data) => {
+      if (err) throw err
+      const obj = JSON.parse(data)
+      if (!obj || !obj['@graph']) throw 'Invalid input data. @graph not found.'
+      obj['@graph'].forEach(element => {
+        this.graph[element['@id']] = element
+      })
+    })
+  }
+
+  get (id) {
+    return this.graph[id]
+  }
+
+  getSuperClasses (id) {
+    let klass = this.graph[id]
+
+    if (!klass || klass['@type'] !== 'rdfs:Class') {
+      return []
+    }
+
+    let classes = [id]
+
+    // zero parents
+    if (!klass['rdfs:subClassOf']) {
+      return classes
+    }
+
+    // one parent
+    if (klass['rdfs:subClassOf']['@id']) {
+      let superSuper = this.getSuperClasses(klass['rdfs:subClassOf']['@id'])
+      return [...new Set(classes.concat(superSuper))] // Merge and unique
+    }
+
+    // multiple parents
+    for (let superClass of klass['rdfs:subClassOf']) {
+      let superSuper = this.getSuperClasses(superClass['@id'])
+      classes = [...superSuper]
+    }
+
+    return [...new Set(classes)]
+  }
+
+  getTypes () {
+    return Object.values(this.graph)
+      .filter(e => e['@type'] && e['@type'] === 'rdfs:Class')
   }
 }
