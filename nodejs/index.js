351e9015134cbfa587ad0f87707794f2bedf4edc
davidglezz <davidglezz@outlook.es>
2018-06-19 10:55:23 +0200

    JS: add cors

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 const SchemaApiRoutes = require('./routes/SchemaApiRoutes')
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8002
 
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use('/', (new SchemaApiRoutes()).getRouter())
