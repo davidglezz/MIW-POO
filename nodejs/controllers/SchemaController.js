c405eb7b82dc1510e0a044976680b2c9bf3e1c6d
davidglezz <davidglezz@outlook.es>
2018-06-19 17:17:08 +0200

    JS: Improve SchemaController class

--- nodejs/controllers/SchemaController.js
+++ nodejs/controllers/SchemaController.js
@@ -1,12 +1,15 @@
+const path = require('path')
 const SchemaObjectStore = require('../classes/SchemaObjectStore')
 const SchemaReader = require('../classes/SchemaReader')
 const SchemaValidator = require('../classes/SchemaValidator')
 
 module.exports = class SchemaController {
   constructor () {
-    this.repository = new SchemaObjectStore()
-    this.schema = new SchemaReader()
-    this.validator = new SchemaValidator()
+    const filepath = path.join(__dirname, '..', '..', 'schema.jsonld')
+    const dbpath = '../data.db'
+    this.repository = new SchemaObjectStore(dbpath)
+    this.schema = new SchemaReader(filepath)
+    this.validator = new SchemaValidator(this.schema)
   }
 
   getAllTypes () {
@@ -17,16 +20,22 @@ module.exports = class SchemaController {
     return this.repository.listTypes()
   }
 
-  create (type, data) {
-    return this.repository.create(type, data)
+  async create (type, data) {
+    let errors = this.validator.validate(data)
+    if (errors.length > 0) return { 'error': errors }
+    this.repository.create(type, JSON.stringify(data))
+    return { 'success': await this.repository.create(type, JSON.stringify(data)) }
   }
 
   read (type, id) {
     return this.repository.read(type, id)
   }
 
-  update (type, id, data) {
-    return this.repository.update(type, id, data)
+  async update (type, id, data) {
+    let errors = this.validator.validate(data)
+    if (errors.length > 0) return { 'error': errors }
+    this.repository.create(type, JSON.stringify(data))
+    return { 'success': await this.repository.update(type, id, JSON.stringify(data)) }
   }
 
   delete (id) {
