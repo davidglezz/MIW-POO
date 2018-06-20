91c0cac8f0279135785798033053bbb02502e36f
davidglezz <davidglezz@outlook.es>
2018-06-21 01:04:37 +0200

    Python: Implement SchemaReader

--- python/SchemaReader.py
+++ python/SchemaReader.py
@@ -1,5 +1,6 @@
 import json
 
+
 class SchemaList:
     def __init__(self):
         try:
@@ -10,18 +11,35 @@ class SchemaList:
 
         if '@graph' not in data:
             raise ValueError('Invalid input data. @graph not found.')
-        
-        self.graph = data
+
+        self.graph = dict(zip([e.get('@id') for e in data], data))
 
     def get(self, id):
-        self.graph[id] if id not in self.graph else False
+        return self.graph[id] if id in self.graph else False
 
     def getSuperClasses(self, id):
-        # TODO
+        klass = self.graph[id] if id in self.graph else False
+
+        if not klass or '@type' not in klass or klass['@type'] != 'rdfs:Class'
+            return list()
+
+        classes = list(id)
+
+        # zero parents
+        if 'rdfs:subClassOf' not in klass
+            return classes
+
+        # one parent
+        if '@id' in klass['rdfs:subClassOf']
+            superSuper = self.getSuperClasses(klass['rdfs:subClassOf']['@id'])
+            return list(set(classes + superSuper))
+
+        # multiple parents
+        for key, superClass in klass['rdfs:subClassOf'].items():
+            classes += self.getSuperClasses(superClass['@id'])
+
+        return list(set(classes)
 
     def getTypes(self, id):
-        # TODO
-
-
-
-
+        return [e for e in self.graph.values() if e['@type'] == 'rdfs:Class']
+        # return list(filter(lambda e: e['@type'] == 'rdfs:Class', self.graph.values()))
