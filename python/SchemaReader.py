e31c7a9a50d0fcdaafef82a7e028c520906cb0d8
davidglezz <davidglezz@outlook.es>
2018-06-21 22:01:16 +0200

    Python: Fix SchemaReader

--- python/SchemaReader.py
+++ python/SchemaReader.py
@@ -1,10 +1,10 @@
 import json
 
 
-class SchemaList:
-    def __init__(self):
+class SchemaReader:
+    def __init__(self, filepath):
         try:
-            with open('../schema.jsonld', 'r') as f:
+            with open(filepath, 'r') as f:
                 data = json.load(f)
         except ValueError:
             raise ValueError('Decoding JSON has failed')
@@ -12,25 +12,25 @@ class SchemaList:
         if '@graph' not in data:
             raise ValueError('Invalid input data. @graph not found.')
 
-        self.graph = dict(zip([e.get('@id') for e in data], data))
+        self.graph = dict([(e['@id'], e) for e in data['@graph'] if '@id' in e])
 
     def get(self, id):
-        return self.graph[id] if id in self.graph else False
+        return self.graph.get(id)
 
     def getSuperClasses(self, id):
         klass = self.graph[id] if id in self.graph else False
 
-        if not klass or '@type' not in klass or klass['@type'] != 'rdfs:Class'
+        if not klass or '@type' not in klass or klass['@type'] != 'rdfs:Class':
             return list()
 
         classes = list(id)
 
         # zero parents
-        if 'rdfs:subClassOf' not in klass
+        if 'rdfs:subClassOf' not in klass:
             return classes
 
         # one parent
-        if '@id' in klass['rdfs:subClassOf']
+        if '@id' in klass['rdfs:subClassOf']:
             superSuper = self.getSuperClasses(klass['rdfs:subClassOf']['@id'])
             return list(set(classes + superSuper))
 
@@ -38,8 +38,10 @@ class SchemaList:
         for key, superClass in klass['rdfs:subClassOf'].items():
             classes += self.getSuperClasses(superClass['@id'])
 
-        return list(set(classes)
+        return list(set(classes))
 
     def getTypes(self, id):
         return [e for e in self.graph.values() if e['@type'] == 'rdfs:Class']
         # return list(filter(lambda e: e['@type'] == 'rdfs:Class', self.graph.values()))
+
+0
