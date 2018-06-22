7c388721aac4c86d0bb4f7e76c0735309e580cb8
davidglezz <davidglezz@outlook.es>
2018-06-22 18:18:25 +0200

    Js: simplify ObjectStore

--- nodejs/classes/SchemaObjectStore.js
+++ nodejs/classes/SchemaObjectStore.js
@@ -13,72 +13,43 @@ module.exports = class SchemaObjectStore {
   async create (type, data) {
     const sql = 'INSERT INTO objects (type, data) VALUES (?, ?)'
     return new Promise((resolve, reject) => {
-      this.db.run(sql, [type, data], (err, data) => {
-        if (err) {
-          reject(err)
-        }
-        resolve(data)
-      })
+      this.db.run(sql, [type, data], (err, data) => resolve(err ? err.code : data))
     })
   }
 
   async read (type, id) {
     const sql = 'SELECT data FROM objects WHERE type = ? AND id = ? LIMIT 1'
     return new Promise((resolve, reject) => {
-      this.db.get(sql, [type, id], (err, data) => {
-        if (err) {
-          reject(err)
-        }
-        resolve(data)
-      })
+      this.db.get(sql, [type, id], (err, data) => resolve(err ? err.code : data))
     })
   }
 
   async update (type, id, data) {
-    const sql = 'UPDATE objects SET data = ? WHERE id = ? AND type = ? LIMIT 1'
+    console.log(id, data)
+    const sql = 'UPDATE objects SET data = ? WHERE id = ?'
     return new Promise((resolve, reject) => {
-      this.db.run(sql, [data, id, type], (err, data) => {
-        if (err) {
-          reject(err)
-        }
-        resolve(data)
-      })
+      this.db.run(sql, [data, id], (err, data) => resolve(err ? err.code : true))
     })
   }
 
   async delete (id) {
     const sql = 'DELETE FROM objects WHERE id = ?'
     return new Promise((resolve, reject) => {
-      this.db.run(sql, [id], (err, data) => {
-        if (err) {
-          reject(err)
-        }
-        resolve(data)
-      })
+      this.db.run(sql, [id], (err, data) => resolve(err ? err.code : true))
     })
   }
 
   async listTypes () {
     const sql = 'SELECT type, COUNT(*) as num FROM objects GROUP BY type'
     return new Promise((resolve, reject) => {
-      this.db.all(sql, [], (err, rows) => {
-        if (err) {
-          reject(err)
-        }
-        resolve(rows)
-      })
+      this.db.all(sql, [], (err, rows) => resolve(err ? err.code : rows))
     })
   }
 
   async findEntities (type) {
     const sql = 'SELECT id, LENGTH(data) AS size FROM objects WHERE type = ?'
     return new Promise((resolve, reject) => {
-      this.db.all(sql, [type], (err, rows) => {
-        if (err) {
-          reject(err)
-        }
-        resolve(rows)
-      })
+      this.db.all(sql, [type], (err, rows) => resolve(err ? err.code : rows))
     })
   }
 
